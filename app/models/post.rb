26d305de966404c5a2de6bb6b522650a4eab881e
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-22 12:40:39 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, exclusion: { in: ["True Facts"] }
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :title, presence: true
+
+#   def title_cannot_allow
+#     # if !title.include? "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+#     #   errors.add(:title, "Title cannot have this values")
+#     # end
+
+#   end
 end
+
+# "Won't Believe"
+# "Secret"
+# "Top [number]"
+# "Guess"
